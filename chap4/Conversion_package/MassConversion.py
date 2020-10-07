c7c2d3952bbe41206488170f915bf888230b1629
Anush Som <anushsom@gmail.com>
2020-10-07 22:13:56 +0530

    Add files via upload

---
+++ chap4/Conversion_package/MassConversion.py
@@ -0,0 +1,26 @@
+def kgtotonne(kg):
+    
+    tonne = kg/1000
+
+    print("It is",tonne,"tonne's")
+
+def tonnetokg(tonne):
+
+    kg = tonne*1000
+    print("It is",kg,"kg's")
+
+
+def kgtopound(kg):
+    
+    pound = kg*2.2042
+
+    print("It is",pound,"pound's")
+
+def poundtokg(pound):
+    
+    kg = pound/2.2042
+
+    print("It is",kg,"kg's")
+
+
+
