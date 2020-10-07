6b0f0f63af5a38e19f76f3310d401cce0db26e0c
Anush Som <anushsom@gmail.com>
2020-10-07 22:16:22 +0530

    Add files via upload

---
+++ lengthconversion.py
@@ -0,0 +1,25 @@
+#this program contains modules which can be used to convert length into various units
+
+def miletokm(mile):
+    
+    km = mile * 1.609344
+
+    print("It is",km,"km's")
+
+def kmtomile(km):
+
+    mile = km / 1.609344
+    print("It is",mile,"miles's")
+
+
+def feettoinches(feet):
+    
+    inches = feet * 12
+
+    print("It is",inches,"inches's")
+
+def inchestofeet(inches):
+
+    feet= inches / 12
+    print("It is",feet,"feet")
+
